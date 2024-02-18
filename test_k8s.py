a403a57cab1f1ba925f86f5cf736cbbeb754f291
Sagar Naik <sagarnaik12@gmail.com>
2024-02-18 23:52:14 +0530

    Update test_k8s.py

--- test_k8s.py
+++ test_k8s.py
@@ -5,20 +5,21 @@ from k8s import K8s
 # Fixture for the K8s instance
 @pytest.fixture
 def k8s_instance():
-    return K8s()
+    k = K8s()
+    return k
 
 def test_get_namespaces(k8s_instance):
     print (k8s_instance.get_namespaces())
-    assert 'kube-system' in K8s.get_namespaces()
+    assert 'kube-system' in k8s_instance.get_namespaces()
 
 def test_get_nodes(k8s_instance):
     print (k8s_instance.get_nodes())
-    assert 'minikube' in K8s.get_nodes()
+    assert 'minikube' in k8s_instance.get_nodes()
 
 def test_get_pods(mock_check_output, k8s_instance):
     print (k8s_instance.get_pods())
-    assert 'ImagePullBackOff' in get_pods()
+    assert 'ImagePullBackOff' in k8s_instance.get_pods()
 
 def test_get_namespaces_error(k8s_instance):
     print (k8s_instance.get_namespaces())
-    assert 'kube-system_error' in K8s.get_namespaces()
+    assert 'kube-system_error' in k8s_instance.get_namespaces()

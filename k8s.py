ce1df0ce6ed11f8f7c33f21f8e094555c0e04ca2
Sagar Naik <sagarnaik12@gmail.com>
2024-02-18 22:27:57 +0530

    Update k8s.py

--- k8s.py
+++ k8s.py
@@ -3,20 +3,20 @@ class K8s:
     def get_namespaces(self):
         res = subprocess.run(["kubectl get namespaces"], shell=True, capture_output=True, text=True)
         #res = subprocess.check_output('sudo kubectl get namespaces', shell=False)
-        print(res)
-        return res
+        print(res.stdout)
+        return res.stdout
 
     def get_nodes(self):
         res = subprocess.run(["kubectl get nodes"], shell=True, capture_output=True, text=True)
         #res = subprocess.check_output('sudo kubectl get nodes', shell=False)
-        print(res)
-        return res
+        print(res.stdout)
+        return res.stdout
 
     def get_pods(self):
         res = subprocess.run(["kubectl get pods"], shell=True, capture_output=True, text=True)
         #res = subprocess.check_output('sudo kubectl get pods', shell=False)
-        print(res)
-        return res
+        print(res.stdout)
+        return res.stdout
 
 k = K8s()
 k.get_namespaces()
